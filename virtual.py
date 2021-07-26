a98020cb9fd9a081221789eb072d1f8a54a0086e
thight1 <thight1@students.kennesaw.edu>
2021-07-26 14:19:18 -0400

    Update virtual.py

--- virtual.py
+++ virtual.py
@@ -1,1 +1,24 @@
-print("Hello world")
+#need prompt for pet type
+#build  pet dictionary 
+#give toys, feed pet, let game loop, while printing menu
+
+
+pet = {"name":"", "type": "", "age": 0, "hunger": 0, "toys": []}
+
+pettoys = {"cat": ["String", "Cardboard Box", "Scratching Post"], "dog": ["Frisbee","Tennis Ball", "Stick"], "fish": ["Undersea Castle", "Buried Treasure", "Coral"]}
+
+def virtpet ():
+    pettype = ""
+
+    petops = list(pettoys.keys())
+
+    while pettype not in petops:
+        print("Please input pet you would like to have:")
+        for option in petops:
+            print(option)
+        pettype = input("Please select from one of these pets: ")
+    
+    print(pettype)
+
+virtpet()
+
